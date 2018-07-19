fa5ab74de13348a0790164ce34d0d4282f77f19c
Yonghui Zhang <comzyh@gmail.com>
2018-07-19 22:14:23 +0800

    0017.letter-combinations-of-a-phone-number

---
+++ 0017.letter-combinations-of-a-phone-number/Solution.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def letterCombinations(self, digits):
+        """
+        :type digits: str
+        :rtype: List[str]
+        """
+        mapping = {
+            "2": "abc",
+            "3": "def",
+            "4": "ghi",
+            "5": "jkl",
+            "6": "mno",
+            "7": "pqrs",
+            "8": "tuv",
+            "9": "wxyz",
+        }
+        ans = [""]
+
+        for d in digits:
+            nextans = []
+            for item in ans:
+                for c in mapping[d]:
+                    nextans.append(item + c)
+            ans = nextans
+        return ans
