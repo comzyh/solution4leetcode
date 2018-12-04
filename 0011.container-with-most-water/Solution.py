4997a856c14d2a9a3c34e500eda6ad4439e0cc95
Comzyh <comzyh@users.noreply.github.com>
2018-12-04 13:30:09 +0800

    Create Solution.py

---
+++ 0011.container-with-most-water/Solution.py
@@ -0,0 +1,17 @@
+class Solution:
+    def maxArea(self, height):
+        """
+        :type height: List[int]
+        :rtype: int
+        """
+        ans = 0
+        l = 0
+        r = len(height) - 1
+        while l < r:
+            ans = max(ans, min(height[l], height[r]) * (r - l))
+            if height[l] < height[r]:
+                l = l + 1
+            else:
+                r = r - 1
+            
+        return ans 
