44b8a8495c4e6033bf85ace8567c989c04decdaf
Comzyh <comzyh@users.noreply.github.com>
2019-02-20 02:59:18 +0800

    0394.decode-string
    0394.decode-string

---
+++ 0394.decode-string/Solution.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    int nextint(const char* &str) {
+        int ans = 0;
+        while (*str >= '0' && *str <= '9') {
+            ans *= 10;
+            ans += *(str ++) - '0';
+        }
+        return ans;
+    }
+    string nextstring(const char * &str) {
+        const char *e = str;
+        for (;*e != ']' && !(*e >= '0' && *e <= '9') && *e != '\0'; e++);
+        string ans(str, e - str);
+        str = e;
+        return ans;
+    }
+    string decode(const char * &str) {
+        string ans;
+        while(*str != '\0' && *str != ']') {
+            int rep = nextint(str);            
+            if (rep == 0) {
+                string repstr = nextstring(str);
+                ans.append(repstr);                
+            } else {
+                str ++; // skip [
+                string repstr = decode(str);
+                while(rep --){
+                    ans.append(repstr);
+                }    
+            }    
+        }
+        if (*str == ']') {
+                str ++;
+        }
+        return ans;
+    }
+    string decodeString(string s) {
+        const char *str = s.data();
+        return decode(str);
+    }
+};
