239364495f28dae5fcbe5cb632ab1ab6d87d6ba6
Comzyh <comzyh@users.noreply.github.com>
2019-02-21 23:58:11 +0800

    0094.binary-tree-inorder-traversal

---
+++ 0094.binary-tree-inorder-traversal/Solution.cpp
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    void inorder(TreeNode* root, vector<int> &ans) {
+        if (root->left) {
+            inorder(root->left, ans);
+        }
+        ans.push_back(root->val);
+        if (root->right) {
+            inorder(root->right, ans);
+        }
+    }
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> ans;
+        if (root != nullptr) {
+            inorder(root, ans);
+        }
+        return ans;
+    }
+};
