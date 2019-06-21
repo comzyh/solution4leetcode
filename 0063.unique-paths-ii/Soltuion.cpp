65ce6a4f16d813ee0c5765433a82e77a324d1a99
Comzyh <comzyh@users.noreply.github.com>
2019-06-21 20:23:48 +0800

    0063.unique-paths-ii

---
+++ 0063.unique-paths-ii/Soltuion.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+        if (obstacleGrid[0][0]) {
+            return 0;
+        }
+        int m = obstacleGrid.size();
+        int n = obstacleGrid[0].size();
+        vector<vector<long long> > dp(m, vector<long long>(n, 0)); // Intermediate  result can overflow
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (obstacleGrid[i][j]) {
+                    continue;
+                }
+                if (i > 0) {
+                    dp[i][j] += dp[i - 1][j];
+                }
+                if (j > 0) {
+                    dp[i][j] += dp[i][j - 1];
+                }
+            }
+        }
+        return dp[m - 1][n - 1];
+    }
+};
