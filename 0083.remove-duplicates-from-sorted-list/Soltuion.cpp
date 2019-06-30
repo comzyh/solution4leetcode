0d73d28f22a9105d44c0c9345bdce21f5e9663a2
Comzyh <comzyh@users.noreply.github.com>
2019-07-01 01:09:44 +0800

    0083.remove-duplicates-from-sorted-list

---
+++ 0083.remove-duplicates-from-sorted-list/Soltuion.cpp
@@ -0,0 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        ListNode **h = &head;
+        while (*h != nullptr) {
+            if ((*h)->next != nullptr && (*h)->next->val == (*h)->val) {
+                ListNode *t = (*h)->next;
+                while (t != nullptr && t->val == (*h)->val) {
+                    t = t->next;
+                }
+                (*h)->next = t;
+            }
+            h = &(*h)->next;
+        }
+        return head;
+    }
+};
